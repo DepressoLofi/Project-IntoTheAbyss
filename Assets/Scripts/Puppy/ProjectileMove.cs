dea1c9e4128e1ed2e6600beff5ec79d7a5e2dcd5
David Mg Mg <125610886+DepressoLofi@users.noreply.github.com>
2024-03-24 19:15:24 +0630

    projectiles fix

--- Assets/Scripts/Puppy/ProjectileMove.cs
+++ Assets/Scripts/Puppy/ProjectileMove.cs
@@ -7,7 +7,20 @@ public class ProjectileMove : MonoBehaviour
 {
     public float speed;
     public float fireRate;
+    public GameObject muzzlePrefab;
 
+    private void Start()
+    {
+        if (muzzlePrefab != null)
+        {
+            var muzzleVFX = Instantiate(muzzlePrefab, transform.position, Quaternion.identity);
+            muzzleVFX.transform.forward = gameObject.transform.forward;
+            Destroy(muzzleVFX, 0.8f);
+
+
+
+        }
+    }
 
     void Update()
     {
@@ -24,6 +37,15 @@ public class ProjectileMove : MonoBehaviour
     void OnCollisionEnter (Collision collision)
     {
         speed = 0;
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector2.up, contact.normal);
+        Vector3 pos = contact.point;
+        var psMuzzle = muzzlePrefab.GetComponent<ParticleSystem>();
+
+        if (muzzlePrefab != null) { 
+            var hitVFX = Instantiate(muzzlePrefab, pos, rot);
+            Destroy(hitVFX, 0.8f);
+        }
 
         Destroy(gameObject);
     }
